74a19cb1454f9fcc62a48ac7416170072fced134
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2024-03-04 21:21:14 +0300

    fix(vite.config): Исправила точку старта приложения

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: resolve(__dirname, 'src'),
+  root: resolve(__dirname),
   build: {
     outDir: resolve(__dirname, 'dist'),
   },
