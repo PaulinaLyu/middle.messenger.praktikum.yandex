673d2bbd9247a05be1807ae45bd1755a3f4fd669
Полина Любезнова <polina.lyubeznova@geogracom.com>
2024-03-04 14:49:29 +0300

    feat: установила vite

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
