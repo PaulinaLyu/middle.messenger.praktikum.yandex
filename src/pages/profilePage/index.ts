a6397327ee31f1e4aad8d28a4255fdb6139c2f8b
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2024-03-12 20:33:39 +0300

    feat(pages/profilePage): Создала шаблон страницы профиля

--- src/pages/profilePage/index.ts
+++ src/pages/profilePage/index.ts
@@ -2,15 +2,14 @@ import Handlebars from "handlebars";
 export { default as profilePage } from "./profilePage.hbs?raw";
 import "./profilePage.scss";
 
-Handlebars.registerHelper("profile-page-content", () => {
-  return [
-    {
-      email: "pochta@yandex.ru",
-      login: "ivanivanov",
-      name: "Иван",
-      first_name: "Иванов",
-      display_name: "Иван",
-      phone: "+7 (909) 967 30 30",
-    },
-  ];
+Handlebars.registerHelper("profile", (context, options) => {
+  const cont = {
+    email: "pochta@yandex.ru",
+    login: "ivanivanov",
+    name: "Иван",
+    first_name: "Иванов",
+    display_name: "Иван",
+    phone: "+7 (909) 967 30 30",
+  };
+  return options.fn(cont);
 });
