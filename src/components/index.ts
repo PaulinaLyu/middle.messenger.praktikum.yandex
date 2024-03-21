4851e711d78dd0bec00b2d37cca652b6530fee99
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2024-03-21 22:29:54 +0300

    feat(components/dropdown): Создала выпадающий список

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,4 +9,7 @@ export { ChatItem } from "./chatItem";
 export { ChatHeader } from "./chatHeader";
 export { ProfileItem } from "./profileItem";
 export { MessagesFeed } from "./messagesFeed";
+export { MessagesFeedHeader } from "./messagesFeedHeader";
 export { Modal } from "./modal";
+export { Dropdown } from "./dropdown";
+export { DropdownOptionItem } from "./dropdownOptionItem";
