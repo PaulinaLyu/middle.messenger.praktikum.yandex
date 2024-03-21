504bba1289e770cedbed1662948f434a33c33f05
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2024-03-21 18:30:26 +0300

    feat(components/chatHeader; components/input): Добавила поиск по чатам

--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -1,2 +1,14 @@
+import Handlebars from "handlebars";
 import "./input.scss";
 export { default as Input } from "./input.hbs?raw";
+
+Handlebars.registerHelper(
+  "if_type_search",
+  function (type: string, options: Handlebars.HelperOptions) {
+    if (type === "search") {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  }
+);
