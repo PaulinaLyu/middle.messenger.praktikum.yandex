91dc57c60c73b65f0825294f5fc2a8e8ffacccf0
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2024-06-11 15:43:20 +0300

    feat: добавила семантические теги на страницу

--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -2,9 +2,18 @@ import Handlebars from "handlebars";
 import "./input.scss";
 export { default as Input } from "./input.hbs?raw";
 
+interface ContextType {
+  type: string;
+  [key: string]: any;
+}
+
 Handlebars.registerHelper(
   "if_type_search",
-  function (type: string, options: Handlebars.HelperOptions) {
+  function (
+    this: ContextType,
+    type: string,
+    options: Handlebars.HelperOptions
+  ) {
     if (type === "search") {
       return options.fn(this);
     } else {
--- src/pages/profilePage/index.ts
+++ src/pages/profilePage/index.ts
@@ -2,13 +2,22 @@ import Handlebars from "handlebars";
 export { default as profilePage } from "./profilePage.hbs?raw";
 import "./profilePage.scss";
 
-Handlebars.registerHelper("if_not_profile", function (a, b, options) {
-  const bothFalse = a === false && b === false;
-  const firstTruesecondFalse = a === true && b === false;
-  debugger;
-  if (bothFalse || firstTruesecondFalse) {
-    return options.fn(this);
-  } else {
-    return options.inverse(this);
+interface ContextType {
+  a?: boolean;
+  b?: boolean;
+  [key: string]: any;
+}
+
+Handlebars.registerHelper(
+  "if_not_profile",
+  function (this: ContextType, a, b, options: Handlebars.HelperOptions) {
+    const bothFalse = !a && !b;
+    const firstTrueSecondFalse = a && !b;
+
+    if (bothFalse || firstTrueSecondFalse) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
   }
-});
+);
