174f9aa4d338210453c82414ac77832ed19c30c2
PaulLyu <polina.lyubeznova@geogracom.com>
2024-06-27 20:40:26 +0300

    refactor(tools/EventBus): Добавила приватный метод checkEvent

--- src/tools/EventBus.ts
+++ src/tools/EventBus.ts
@@ -15,6 +15,12 @@ export default class EventBus {
     return !!this.listeners[event];
   }
 
+  private checkEvent(event: string): void {
+    if (!this.hasEvent(event)) {
+      throw new Error(`Нет события: ${event}`);
+    }
+  }
+
   on(event: string, callback: Listener): void {
     if (!this.hasEvent(event)) {
       this.listeners[event] = [];
@@ -24,18 +30,13 @@ export default class EventBus {
   }
 
   off(event: string, callback: Listener): void {
-    if (!this.hasEvent(event)) {
-      throw new Error(`Нет события: ${event}`);
-    }
+    this.checkEvent(event);
     const filteredListeners = this.listeners[event].filter(listener => listener !== callback);
     this.listeners[event] = filteredListeners;
   }
 
   emit(event: string, ...args: unknown[]): void {
-    if (!this.listeners[event]) {
-      throw new Error(`Нет события: ${event}`);
-    }
-
+    this.checkEvent(event);
     this.listeners[event].forEach(listener => {
       listener(...args);
     });
