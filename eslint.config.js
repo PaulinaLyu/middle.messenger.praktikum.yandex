b17936519782d69a88f59e9b9baf7f62c4576d38
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2024-07-24 21:24:51 +0300

    feat(eslint): настроила eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: {...globals.browser, ...globals.node} },
     rules: {
       "prettier/prettier": ["warn", { endOfLine: "auto" }],
     },
+    plugins: {
+      prettier: prettierPlugin,
+    }
+  },
+  {
+    ignores: ['node_modules', 'dist']
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    
+  }
 ];
