9451c2701c4850a722a38ddca1d06d3810877894
PaulLyu <polina.lyubeznova@geogracom.com>
2025-02-06 23:52:17 +0300

    исправила конфигурацию viteи настройку сервера

--- server.mjs
+++ server.mjs
@@ -7,7 +7,8 @@ const port = 3000;
 const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("/", (req, res) => {
+
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
